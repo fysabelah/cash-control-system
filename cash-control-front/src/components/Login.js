369ecbbc32f63396dcc0b4e160948c458b193ad1
Isabela França <fysabelah@gmail.com>
2024-10-06 17:59:31 -0300

    - Formulário login em andamento

--- cash-control-front/src/components/Login.js
+++ cash-control-front/src/components/Login.js
@@ -1,27 +1,67 @@
 import React from "react";
+import "../styles/Login.css";
 
 export default function Login() {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function updateUsername(event) {
+        setUsername(event.target.value);
+    }
+
+    function updatePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function sendLoginRequest() {
+        const response = fetch('/api/user', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: btoa(password)
+            })
+        })
+
+        console.log(response);
+
+        alert(response);
+
+        /*.then((response) => {
+            if (response.ok) {
+                console.log(response);
+            } else {
+                console.log('deu ruim ');
+                console.log(response)
+                console.log(response)
+            }
+        })*/
+    }
+
     return (
         <div className="Login">
             <form className="LoginForm">
-                <div>
+                <div className="LoginInputs">
                     <div>
                         <p>Usuário</p>
                     </div>
                     <div>
-                        <input type={"text"}/>
+                        <input type={"text"} name="usuario" onChange={updateUsername}/>
                     </div>
                 </div>
-                <div>
+                <div className="LoginInputs">
                     <div>
                         <p>Senha</p>
                     </div>
                     <div>
-                        <input type={"password"}/>
+                        <input name="password" type={"password"} onChange={updatePassword}/>
                     </div>
                 </div>
-                <div>
-                    <button>Enviar</button>
+                <div className="ButtonDiv">
+                    <button onClick={sendLoginRequest}>Enviar</button>
                 </div>
             </form>
         </div>
